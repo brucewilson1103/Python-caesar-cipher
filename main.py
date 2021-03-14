bf8972cf23989f7c3bbadc28a28df5a0ed9094f2
Bruce Wilson <brucewilson1103@gmail.com>
2021-03-14 23:29:35 +0000

    condense logic and code eliminate repetition

--- main.py
+++ main.py
@@ -6,23 +6,14 @@ text = input("Type your message:\n").lower()
 # The shift is the agreed upon value between the sender and reciever
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def cipher(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+    end_text += alphabet[new_position]
+  print(f"Here's the {direction}d result: {end_text}")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
-
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+cipher(start_text=text, shift_amount=shift, cipher_direction=direction)
