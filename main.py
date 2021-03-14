71ece99bd66257cea70e9b3c0f0ebe06f35aa29e
Bruce Wilson <brucewilson1103@gmail.com>
2021-03-14 23:43:24 +0000

    exit message

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ while not should_end:
   restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
   if restart == "no":
     should_end = True
-    print("Goodbye")
+    print("Godspeed")
     
 
 
